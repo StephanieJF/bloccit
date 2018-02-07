a971a7498992e6a0418c4477066b90312e350d4a
Stephanie Fischler <sjfischler@gmail.com>
2018-02-07 18:23:17 -0500

    WIP 3

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,10 +8,7 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = Question.new
-    @question.title = params[:question][:title]
-    @question.body = params[:question][:body]
-
+    @question = Question.new(params.require(:question).permit(:title, :body, :resolved))
     if @question.save
       flash[:notice] = "Question was saved."
       redirect_to @question
@@ -22,14 +19,36 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @question = Question.find(params[:id])
   end
 
   def edit
+    @question = Question.find(params[:id])
   end
 
   def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+
+    if @question.save
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash.now[:alert] = "There was an error saving the question. Please try again."
+      render :edit
+    end
   end
 
   def destroy
+    @question = Question.find(params[:id])
+
+    if @question.destroy
+      flash[:notice] = "\"#{@question.title}\" was deleted successfully."
+      redirect_to questions_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
   end
 end
