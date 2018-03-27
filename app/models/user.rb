0956fd4471536361e6b421393663ec3611d438a9
Stephanie Fischler <sjfischler@gmail.com>
2018-03-27 07:16:50 -0400

    WIP 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
   before_save { self.email = email.downcase if email.present? }
   before_save { self.role ||= :member }
 
@@ -8,10 +9,10 @@ class User < ApplicationRecord
 
     validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
     validates :password, length: { minimum: 6 }, allow_blank: true
-     validates :email,
-               presence: true,
-               uniqueness: { case_sensitive: false },
-               length: { minimum: 3, maximum: 254 }
+    validates :email,
+              presence: true,
+              uniqueness: { case_sensitive: false },
+              length: { minimum: 3, maximum: 254 }
 
      has_secure_password
 
